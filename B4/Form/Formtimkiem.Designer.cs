a8958597917bd0ec38f256394e12ea154a8d6770
hitekvn <31841044+hitekvn@users.noreply.github.com>
2017-10-11 07:42:29 +0700

    lană

--- B4/Form/Formtimkiem.Designer.cs
+++ B4/Form/Formtimkiem.Designer.cs
@@ -28,20 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Ma_hh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ten_hh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Don_vi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ma_hh,
+            this.Ten_hh,
+            this.Don_vi});
+            this.dataGridView1.Location = new System.Drawing.Point(5, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(547, 237);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Ma_hh
+            // 
+            this.Ma_hh.DataPropertyName = "Ma_hh";
+            this.Ma_hh.HeaderText = "Mã Hàng Hóa";
+            this.Ma_hh.Name = "Ma_hh";
+            // 
+            // Ten_hh
+            // 
+            this.Ten_hh.DataPropertyName = "Ten_hh";
+            this.Ten_hh.HeaderText = "Tên Hàng Hóa";
+            this.Ten_hh.Name = "Ten_hh";
+            // 
+            // Don_vi
+            // 
+            this.Don_vi.DataPropertyName = "Don_vi";
+            this.Don_vi.HeaderText = "Đơn Vị";
+            this.Don_vi.Name = "Don_vi";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Tất cả";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Lọc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 23);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "DANH SÁCH HÀNG HÓA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 16;
             // 
             // Formtimkiem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(557, 349);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Formtimkiem";
             this.Text = "Tìm Kiếm";
             this.Load += new System.EventHandler(this.Formtimkiem_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ma_hh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ten_hh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Don_vi;
     }
 }
--- B4/Form/Formtimkiem.cs
+++ B4/Form/Formtimkiem.cs
@@ -18,10 +18,41 @@ namespace B4
         public Formtimkiem()
         {
             InitializeComponent();
+            load();
         }
+
+        void load()
+        {
+            db.Hangs.Load();
+            dataGridView1.DataSource = db.Hangs.Local;
+        }
+
         private void Formtimkiem_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        public IQueryable<Hang> timtatca(string find)
+        {
+            return db.Hangs.Where(p => p.Ten_hh.Contains(find));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string aa = textBox1.Text.Trim();
+
+            List<Hang> aaa = timtatca(aa).ToList();
+
+            dataGridView1.DataSource = aaa;
         }
     }
 }
