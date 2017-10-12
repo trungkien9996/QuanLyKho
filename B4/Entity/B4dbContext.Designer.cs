1541a0cc240c7d571866eb3438ceb210365bdc1c
doantanmy96 <dtm96qn@gmail.com>
2017-10-12 08:49:50 +0700

    lan1

--- B4/Entity/B4dbContext.Designer.cs
+++ B4/Entity/B4dbContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\Công nghệ web\MVC\B4\B4\Entity\B4dbContext.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\GITHUB\QuanLyKho\B4\Entity\B4dbContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- B4/Form/FormXuat.cs
+++ B4/Form/FormXuat.cs
@@ -17,6 +17,98 @@ namespace B4
         public FormXuat()
         {
             InitializeComponent();
+            loadcb();
+        }
+
+        public void loadcb()
+        {
+
+
+            comboBox3.DataSource = db.Khoes.ToList();
+            comboBox3.DisplayMember = "Ten_kho";
+            comboBox3.ValueMember = "Ma_kho";
+
+            comboBox1.DataSource = db.PhieuXuats.ToList();
+            comboBox1.DisplayMember = "Ma_px";
+            comboBox1.ValueMember = "Ma_px";
+
+        }
+
+        void add()
+        {
+            if (tb_mahang.Text == "" || tb_soluong.Text == "") MessageBox.Show("Mã hh và số lượng không đc để trống", "Cảnh báo");
+            else
+            {
+                try
+                {
+                    Detail_PX kho = new Detail_PX() { Ma_px = comboBox1.SelectedValue.ToString(), Ma_kho = comboBox1.SelectedValue.ToString(), Ma_hh = tb_mahang.Text };
+                    db.Detail_PX.Add(kho);
+                    db.SaveChanges();
+                    MessageBox.Show("Thêm mới thành công", "Message");
+                    tb_mahang.Text = "";
+                    tb_soluong.Text = "";
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.ToString(), "ERROR");
+                }
+
+            }
+        }
+        //void edit()
+        //{
+        //    if (tb_mahh.Text == "" || tb_tenhh.Text == "") MessageBox.Show("Mã hh và tên hh không đc để trống", "Cảnh báo");
+        //    else
+        //    {
+        //        try
+        //        {
+        //            string id = tb_mahh.Text;
+        //            Hang sv = db.Hangs.Find(id);
+        //            sv.Ten_hh = tb_tenhh.Text;
+        //            sv.Don_vi = tb_donvi.Text;
+        //            db.SaveChanges();
+        //            MessageBox.Show("Sửa thành công", "Message");
+        //            tb_mahh.Text = "";
+        //            tb_tenhh.Text = "";
+        //            tb_donvi.Text = "";
+        //        }
+        //        catch (Exception exp)
+        //        {
+        //            MessageBox.Show(exp.ToString(), "ERROR");
+        //        }
+        //    }
+
+        //}
+        //void del()
+        //{
+        //    if (tb_mahh.Text == "" || tb_tenhh.Text == "") MessageBox.Show("Mã hh và tên hh không đc để trống", "Cảnh báo");
+        //    else
+        //    {
+        //        try
+        //        {
+        //            string id = tb_mahh.Text;
+        //            Hang sv = db.Hangs.Where(p => p.Ma_hh == id && p.Ten_hh == tb_tenhh.Text && p.Don_vi == tb_donvi.Text).SingleOrDefault();
+        //            db.Hangs.Remove(sv);
+        //            db.SaveChanges();
+        //            tb_mahh.Text = "";
+        //            tb_tenhh.Text = "";
+        //            tb_donvi.Text = "";
+        //        }
+        //        catch (Exception exp)
+        //        {
+        //            MessageBox.Show(exp.ToString(), "ERROR");
+        //        }
+        //    }
+        //}
+
+        private void bt_xuất_Click(object sender, EventArgs e)
+        {
+            add();
+        }
+
+        private void bt_thoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
