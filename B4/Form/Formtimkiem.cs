e3c87b928ea0453c34e8e5ed9a9435420290b2b6
hitekvn <31841044+hitekvn@users.noreply.github.com>
2017-10-17 15:46:39 +0700

    c

--- B4/Form/Formtimkiem.cs
+++ B4/Form/Formtimkiem.cs
@@ -27,7 +27,20 @@ namespace B4
             dataGridView1.DataSource = db.Hangs.Local;
         }
 
-    
+        private void Formtimkiem_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
         public IQueryable<Hang> timtatca(string find)
         {
             return db.Hangs.Where(p => p.Ten_hh.Contains(find));
