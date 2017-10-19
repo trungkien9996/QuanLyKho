595b45a630cd951d4eeb0fe0349db2178c182e11
trungkien9996 <31985809+trungkien9996@users.noreply.github.com>
2017-10-19 20:30:06 +0700

    ad

--- B4/Form/Form1.cs
+++ B4/Form/Form1.cs
@@ -22,59 +22,6 @@ namespace B4
         {
             if (MessageBox.Show("Do you want to quit?", "Message", MessageBoxButtons.OKCancel) == DialogResult.Cancel) e.Cancel = true;
         }
-
-
-        //public void loadcb()
-        //{
-        //    comboBox1.DataSource = db.Khoes.ToList();
-        //    comboBox1.DisplayMember = "Ten_kho";
-        //    comboBox1.ValueMember = "Ma_kho";
-        //}
-
-        //void add()
-        //{
-        //    if (tb_maPN.Text == "") MessageBox.Show("Mã pn không đc để trống", "Cảnh báo");
-        //    else
-        //    {
-        //        try
-        //        {
-        //            PhieuNhap kho = new PhieuNhap() { Ma_PN = tb_maPN.Text, Ma_kho = comboBox1.SelectedValue.ToString(), Ngay_nhap = Convert.ToDateTime(dateTimePicker1.Text) };
-        //            db.PhieuNhaps.Add(kho);
-        //            db.SaveChanges();
-        //            MessageBox.Show("Thêm mới thành công", "Message");
-        //            tb_maPN.Text = "";
-        //        }
-        //        catch (Exception exp)
-        //        {
-        //            MessageBox.Show(exp.ToString(), "ERROR");
-        //        }
-
-        //    }
-        //}
-        ////void edit()
-        ////{
-        ////    if (tb_mahh.Text == "" || tb_tenhh.Text == "") MessageBox.Show("Mã hh và tên hh không đc để trống", "Cảnh báo");
-        ////    else
-        ////    {
-        ////        try
-        ////        {
-        ////            string id = tb_mahh.Text;
-        ////            Hang sv = db.Hangs.Find(id);
-        ////            sv.Ten_hh = tb_tenhh.Text;
-        ////            sv.Don_vi = tb_donvi.Text;
-        ////            db.SaveChanges();
-        ////            MessageBox.Show("Sửa thành công", "Message");
-        ////            tb_mahh.Text = "";
-        ////            tb_tenhh.Text = "";
-        ////            tb_donvi.Text = "";
-        ////        }
-        ////        catch (Exception exp)
-        ////        {
-        ////            MessageBox.Show(exp.ToString(), "ERROR");
-        ////        }
-        ////    }
-
-        ////}
         private void button1_Click(object sender, EventArgs e)
         {
             string user = textBox1.Text;
